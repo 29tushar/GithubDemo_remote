8cbbd3db0e7a57ca004c7bf2535ed587d3895a8b
Tushar Gupta <tusharsa001@ntu.edu.sg>
2023-08-21 16:21:08 +0800

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Tushar 19 on 21/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
