28c479fa7cf08921f9eecfb6145f4636b525363d
Tushar Gupta <tusharsa001@ntu.edu.sg>
2023-08-21 16:31:30 +0800

    Create SecondView

---
+++ GithubDemo/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GithubDemo
+//
+//  Created by Tushar 19 on 21/8/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
