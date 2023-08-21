59863bfe0acb55159b7d36491c6592cefec2d4e7
Tushar Gupta <tusharsa001@ntu.edu.sg>
2023-08-21 16:23:50 +0800

    Changed the output as well added some comments.

--- GithubDemo/ContentView.swift
+++ GithubDemo/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Body 
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello World")
+            Text("This is a test")
         }
         .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
